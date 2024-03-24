fcd6822a81471f13bbdd4c798a7500ac09a6521d
sao-coding <jeter.nice@gmail.com>
2024-03-24 15:14:12 +0800

    ✨ 新增 登入機制 webview 偵測

--- src/app/(main)/sign-in/login-button.tsx
+++ src/app/(main)/sign-in/login-button.tsx
@@ -11,12 +11,7 @@ const LoginButton = () => {
   const [isWebView, setIsWebView] = React.useState<boolean | string>("loading")
 
   React.useEffect(() => {
-    setIsWebView(
-      isUaWebView(
-        "Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/28.0.0.20.16;]"
-      )
-      // isUaWebView(window.navigator.userAgent)
-    )
+    setIsWebView(isUaWebView(window.navigator.userAgent))
   }, [])
 
   return (
