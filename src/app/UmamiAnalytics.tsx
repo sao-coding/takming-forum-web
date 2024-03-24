cca8d53f0f53bb271856dd96cf6762b03d08ae38
sao-coding <jeter.nice@gmail.com>
2024-03-25 02:19:23 +0800

    ✨ 新增 網站流量監測

--- src/app/UmamiAnalytics.tsx
+++ src/app/UmamiAnalytics.tsx
@@ -1,12 +1,12 @@
 import Script from "next/script"
 
 const UmamiAnalytics = () => {
-  // <script defer src="https://umami.sao-x.com/script.js" data-website-id="95225a9d-389b-4c3f-af77-92a923318e41"></script>
+  // <script defer src="https://umami.sao-x.com/script.js" data-website-id="13eafc7d-a9b1-4159-8ca5-fb892eb5b2a0"></script>
   return (
     <Script
       async
       src='https://umami.sao-x.com/script.js'
-      data-website-id='95225a9d-389b-4c3f-af77-92a923318e41'
+      data-website-id='13eafc7d-a9b1-4159-8ca5-fb892eb5b2a0'
     />
   )
 }
