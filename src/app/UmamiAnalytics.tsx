91aba1c00635524226292be4cf4d8de368acd90f
sao-coding <jeter.nice@gmail.com>
2024-05-17 04:45:36 +0800

    🔧 修改 Umami 分析代碼

--- src/app/UmamiAnalytics.tsx
+++ src/app/UmamiAnalytics.tsx
@@ -1,12 +1,11 @@
 import Script from "next/script"
 
 const UmamiAnalytics = () => {
-  // <script defer src="https://umami.sao-x.com/script.js" data-website-id="13eafc7d-a9b1-4159-8ca5-fb892eb5b2a0"></script>
   return (
     <Script
       async
       src='https://umami.sao-x.com/script.js'
-      data-website-id='13eafc7d-a9b1-4159-8ca5-fb892eb5b2a0'
+      data-website-id='7fa5b144-c4ec-4954-be98-6d81679244d6'
     />
   )
 }
