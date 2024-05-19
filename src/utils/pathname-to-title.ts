3555cb28d76a99f1343bd8e0188bb8eff2222620
sao-coding <jeter.nice@gmail.com>
2024-05-19 23:57:36 +0800

    🐛 修復 log api 崩潰

--- src/utils/pathname-to-title.ts
+++ src/utils/pathname-to-title.ts
@@ -3,13 +3,15 @@ import { prisma } from "@/lib/prisma"
 const pathnameToTitle = async (pathname: string) => {
   let title
 
-  if (pathname.startsWith("/teacher") && title === undefined) {
+  if (pathname.startsWith("/teacher")) {
     if (pathname.split("/").length === 3) {
+      console.log("pathname3", pathname.split("/")[2])
       title = await prisma.teacher.findUnique({
         where: { id: pathname.split("/")[2] }
       })
       title = title?.name
-    } else {
+    } else if (pathname.split("/").length === 4) {
+      console.log("pathname4", pathname.split("/")[4])
       title = await prisma.course.findUnique({
         where: { id: pathname.split("/")[4] }
       })
